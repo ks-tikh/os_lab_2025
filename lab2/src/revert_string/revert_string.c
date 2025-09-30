4bcb0b30f667444d064e0c760ca393ef8f2ef054
ks-tikh <tixomkd@gmail.com>
2025-09-30 11:33:47 +0300

    Изменение функции - Реализация отзеркаливание входной строки

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int length = strlen(str);
+    int i, j;
+    char tmp;
+    
+    for (i = 0, j = length - 1; i < j; i++, j--)
+    {
+        tmp = str[i];
+        str[i] = str[j];
+        str[j] = tmp;
+    }
 }
 
